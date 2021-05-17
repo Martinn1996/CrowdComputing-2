dfc34cd2780ef842c54ec03a5cc15a8b88262a0b
Quentin Lee <Quentin.c.l.lee@gmail.com>
2021-05-17 18:27:04 +0200

    Add routing, bootstrap

---
+++ src/components/Task1/Task1.js
@@ -0,0 +1,3 @@
+export default function Task1() {
+    return <h1>Task 1</h1>
+}
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Route, HashRouter } from 'react-router-dom';
+import Task1 from './components/Task1/Task1';
+import Home from './components/Home/Home';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<HashRouter >
+			<Route exact path="/" component={Home} />
+			<Route exact path="/task1" component={Task1} />
+		</HashRouter>
+	</React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
